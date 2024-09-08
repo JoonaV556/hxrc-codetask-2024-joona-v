6f9fb4ca2da20b09162e57ec7656db45bb99d6b8
JoonaV556 <joona.virmajoki@gmail.com>
2024-09-08 20:22:51 +0300

    added size changing for stars

--- Assets/Scripts/SizeChanger.cs
+++ Assets/Scripts/SizeChanger.cs
@@ -7,5 +7,49 @@ using UnityEngine;
 /// </summary>
 public class SizeChanger : MonoBehaviour
 {
+    public float ScaleSmall, ScaleBig;
 
+    public float ScaleChangePerSecond = 0.1f;
+
+    public float directionMultiplier;
+
+    private void Update()
+    {
+        // Decide which way to scale
+        directionMultiplier = GetScaleDirectionMultiplier();
+
+        // Sync to framerate & modify with direction
+        float actualScaleChange = ScaleChangePerSecond * Time.deltaTime * directionMultiplier;
+
+        // Modify size
+        transform.localScale = new Vector3(
+            transform.localScale.x + actualScaleChange,
+            transform.localScale.y + actualScaleChange,
+            transform.localScale.z + actualScaleChange
+        );
+    }
+
+    /// <summary>
+    /// Returns which way scaling should happen
+    /// </summary>
+    float directionMultiplierLastFrame = 1f;
+    float GetScaleDirectionMultiplier()
+    {
+        if (transform.localScale.x > ScaleBig)
+        {
+            // Time to shrink down
+            return directionMultiplierLastFrame = -1f;
+        }
+        else if (transform.localScale.x < ScaleSmall)
+        {
+            // Time to grow
+            return directionMultiplierLastFrame = 1;
+        }
+        else
+        {
+            // Limits not reached, continue scaling same way as last frame
+            return directionMultiplierLastFrame;
+        }
+
+    }
 }
