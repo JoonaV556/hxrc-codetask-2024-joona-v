94e9ba811a7b15fd91677c7df04798560770d793
JoonaV556 <joona.virmajoki@gmail.com>
2024-09-11 16:21:04 +0300

    made playercoloragent singleton, required for fixing bug with obstacles spawning white

--- Assets/Scripts/ColorSystems/PlayerColorAgent.cs
+++ Assets/Scripts/ColorSystems/PlayerColorAgent.cs
@@ -1,10 +1,31 @@
 using System;
-using System.Drawing;
+using UnityEngine;
 
 public class PlayerColorAgent : ColorAgent
 {
+    public static PlayerColorAgent instance; // singleton is required so obstacles can easily fetch player color when they are spawned, and use it to randomize child component colors
     public static Action OnCollidedWithWrongColorEvent;
     public static Action<ColorDataBase.ColorKey> OnPlayerColorSwitched;
+
+    private void Awake()
+    {
+        // init singleton 
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Duplicate PlayerColorAgent instance detected destroying other one");
+            Destroy(instance);
+            instance = this;
+        }
+    }
+
+    public static ColorDataBase.ColorKey GetPlayerColor()
+    {
+        return instance.Color;
+    }
 
     /// <summary>
     /// Call this function from colorized obstacles if colors dont match
